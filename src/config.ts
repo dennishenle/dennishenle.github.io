3dcce4756fdc4067ea2dfe242a75c281924b2557
Dennis Henle <henledennis@gmail.com>
2026-03-17 19:26:24 +0100

    feat(dark-mode): added flag in config.ts and implemented new style class

--- src/config.ts
+++ src/config.ts
@@ -10,6 +10,7 @@ export const siteConfig = {
   title: "Mobile Developer",
   description: "Portfolio website of Dennis Henle",
   accentColor: "#761A5F",
+  darkMode: false,
   social: {
     email: "henledennis@gmail.com",
     github: "https://github.com/dennishenle",
