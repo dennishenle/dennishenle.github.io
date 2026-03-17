f068b2d71f0ab14181f228e5024b289bb8b25699
Dennis Henle <henledennis@gmail.com>
2026-03-17 17:59:33 +0100

    feat: added workflow to push onto github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://dennishenle.github.io",
   vite: {
     plugins: [tailwindcss()],
   },
